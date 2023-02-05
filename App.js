4a489a68aca4f01c5034f3868143e4b5d27d6345
Joao Jamba <jambapedrojoao@gmail.com>
2023-02-05 01:16:09 +0100

    feact: create progressive bar and add the subject focos in timer components

--- App.js
+++ App.js
@@ -1,36 +1,36 @@
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View, Platform } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View, Platform } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
 
-import { colors, Colors } from './src/utils/colors';
-import { Pomodoro } from './src/features/Pomodoro';
-import { Timer } from './src/features/Timer';
+import { colors, Colors } from "./src/utils/colors";
+import { Pomodoro } from "./src/features/Pomodoro";
+import { Timer } from "./src/features/Timer";
 
 export default function App() {
-  const [currentSubject, setCurrentSubject] = useState(null)
+  const [currentSubject, setCurrentSubject] = useState(null);
 
   return (
-    <SafeAreaView style={styles.container}>
-      {!currentSubject? 
-        (
-          <Pomodoro addSubject={setCurrentSubject}/>
-        ): (
+    <PaperProvider>
+      <SafeAreaView style={styles.container}>
+        {!currentSubject ? (
+          <Pomodoro addSubject={setCurrentSubject} />
+        ) : (
           <Timer
             focusSubject={currentSubject}
-            onTimerEnd={()=> {}}
-            clearSubject={()=> {}}
+            onTimerEnd={() => {}}
+            clearSubject={() => {}}
           />
-        )
-      }
-    </SafeAreaView>
+        )}
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight: 0,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     backgroundColor: colors.dackblue,
   },
 });
